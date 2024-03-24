7ee4effdee169cd9eb29fc56452ee30861882372
MasterFat <sd67fg89@gmail.com>
2024-03-25 03:02:21 +0800

    docs: 新增 RWD CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ export default {
         Work_Sans: ["Work Sans", "sans-serif"],
         Space_Mono: ["Space Mono", "monospace"],
       },
+      screens: {
+        tablet: "834px",
+        desktop: "1280px",
+      },
     },
     plugins: [],
   },
