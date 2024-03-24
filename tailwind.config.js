215f5408e9214a8f5df7dd17d20f6bd478458cfc
MasterFat <sd67fg89@gmail.com>
2024-03-24 22:46:21 +0800

    feat: 新增Space Mono字體

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+        Work_Sans: ["Work Sans", "sans-serif"],
+        Space_Mono: ["Space Mono", "monospace"],
       },
     },
     plugins: [],
